0e7e294c6205d6e778af280a693d6f90407a1f67
alexandruarmas <alexandruarmas@gmail.com>
2025-05-12 06:24:20 +0300

    Add GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portv2',
+  assetPrefix: '/portv2/',
+};
 
 export default nextConfig;
